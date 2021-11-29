1f92cf5cae83cb80d50a8cb97a81223c7ad40ab0
Денис Афанасьев <hodache@yandex.ru>
2021-11-29 22:13:20 +0800

    Добавьте файлы проекта.

---
+++ LAB5/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LAB5.Objects;
+
+namespace LAB5
+{
+    public partial class Form1 : Form
+    {
+        List<BaseObject> objects = new List<BaseObject>();
+        Player player;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+            objects.Add(player);
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.Clear(Color.White);
+
+            foreach(var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+
+        }
+    }
+}
---
+++ LAB5/Objects/BaseObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+
+namespace LAB5.Objects
+{
+    class BaseObject
+    {   
+        private float X;
+        private float Y;
+        private float Angle;
+
+        public BaseObject(float x, float y, float angle)
+        {
+            X = x;
+            Y = y;
+            Angle = angle;
+        }
+
+        public Matrix GetTransform()
+        {
+            var matrix = new Matrix();
+            matrix.Translate(X, Y);
+            matrix.Rotate(Angle);
+
+            return matrix;
+        }
+
+        public virtual void Render(Graphics g)
+        {
+
+        }
+    }
+}
